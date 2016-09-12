68d5a5b94b29149b5d8259ba0b6b9083cfc6650b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2016-09-11 21:38:37 -0700

    Collectibles now set title and meta description.

--- client/app/routes/collectible/view/view.component.ts
+++ client/app/routes/collectible/view/view.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 // Models.
 import { AlertMessage } from '../../../models/alertMessage';
 import { Collectible } from '../../../models/collectible';
 // Services.
+import { Meta } from '../../../services/dom/meta/meta.provider';
 import { CollectibleService } from '../../../services/collectible/collectible.service';
 // Components.
 import { UserImages, Options as UserImagesOptions } from '../../../components/users/images/images.component';
@@ -16,7 +18,9 @@ import { CollectibleFull, Options as CollectibleOptions } from '../../../compone
     templateUrl: 'view.html',
     styleUrls: ['view.css'],
     providers: [
-        CollectibleService
+        CollectibleService,
+        Title,
+        Meta
     ],
     directives: [
         CollectibleFull
@@ -29,14 +33,17 @@ export class RoutesCollectibleView implements OnInit {
     collectibleOptions: CollectibleOptions = new CollectibleOptions();
     working: boolean = false;
     loaded: boolean = false;
-    constructor(private route: ActivatedRoute, private collectibleService: CollectibleService) {
-        this.collectibleId = route.snapshot.params['cId'];        
+    constructor(private route: ActivatedRoute, private collectibleService: CollectibleService,
+                private title: Title, private meta: Meta) {
+        this.collectibleId = route.snapshot.params['cId'];
     }
     ngOnInit() {
         this.working = true;
         this.collectibleService.read(this.collectibleId).subscribe(
             collectible => {
                 this.collectible = collectible;
+                this.title.setTitle(this.collectible.name);
+                this.meta.setMeta('description', this.collectible.description);
                 this.loaded = true;
             },
             err => this.alerts.push({ type: 'error', message: err }),
