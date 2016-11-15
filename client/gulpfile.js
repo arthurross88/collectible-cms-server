7cb6b342f54cf41b197933dfbbddf651c67d2dc5
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2016-11-14 16:31:40 -0800

    fixing packaging.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -2,27 +2,20 @@ var gulp  = require('gulp');
 var clean = require('gulp-clean');
 var exec  = require('child_process').exec;
 
-var assetFiles = [
-    './app/**/*.html',
-    './app/**/*.css'
-];
-
 gulp.task('clean', function(){
-    return gulp.src(['dist/aot/*'], {read:false})
-               .pipe(clean());
-});
-
-gulp.task('ngc', ['clean'], function(cb) {
-    return exec('.\\node_modules\\.bin\\ngc --p ./tsconfig.aot.json', function(err, stdout, stderr) {
+    return exec('rimraf dist', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         cb(err);
-    })
+    });
 });
 
-gulp.task('move', ['clean', 'ngc'], function() {
-    return gulp.src(assetFiles, { base: './' })
-               .pipe(gulp.dest('dist/aot'))
+gulp.task('build', ['clean'], function(cb) {
+    return exec('webpack --config config/webpack.prod.js --progress --profile --bail', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
 });
 
-gulp.task('default', ['clean', 'ngc', 'move']);
+gulp.task('default', ['clean', 'build']);
